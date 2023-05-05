120610c49ec7d0622a1afc28264c0525e8113ce1
victorglim <victor.guedes888@gmail.com>
2023-05-05 03:48:12 -0300

    feat: the address model has been added for each user where it is possible to validate the zip code, complement, number, and street fields

--- addresses/models.py
+++ addresses/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    number = models.IntegerField()
+    complement = models.IntegerField()
+    neighborhood = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=20)
+    
+    user = models.OneToOneField(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
