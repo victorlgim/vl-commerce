b92cc4abc4f2ca11bcae8f5cbab1ab1c74f7b17c
victorglim <victor.guedes888@gmail.com>
2023-05-05 03:28:14 -0300

    feat: the user model has been created and features have been added to enable instantiation of a boolean for customer, admin, or seller

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11, unique=True)
+    is_seller = models.BooleanField(default=False, blank=True)
+    is_superuser = models.BooleanField(default=False, blank=True)
+    is_costumer = models.BooleanField(default=True, blank=True)
+    email = models.EmailField(max_length=127, unique=True)
+    username = models.CharField(max_length=30, blank=True, null=True)
