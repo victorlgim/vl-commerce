fc9f2e363db7be720ed6dba3ed2c076bfe14f48e
victorglim <victor.guedes888@gmail.com>
2023-05-05 04:06:28 -0300

    feat: the order model was created establishing a direct relationship with the user, with a text choice class inputted for each order status

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class StatusOrder(models.TextChoices):
+    PEDIDO_REALIZADO = "Pedido Realizado"
+    PEDIDO_EM_ANDAMENTO = "Pedido em Andamento"
+    ENTREGUE = "Entregue"
+
+
+class Order(models.Model):
+    status = models.CharField(
+        max_length=20, choices=StatusOrder.choices, default=StatusOrder.PEDIDO_REALIZADO
+    )
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    total_quantity = models.PositiveIntegerField(default=0)
+    buyed_at = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    users = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="orders"
+    )
