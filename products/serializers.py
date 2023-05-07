2d64833aa929dc81e7d93db33e9baba1a8fc5413
victorglim <victor.guedes888@gmail.com>
2023-05-07 20:13:38 -0300

    feat: created the product serializer by passing the fields from the category model

--- products/serializers.py
+++ products/serializers.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 from .models import Product, Category
 from users.serializers import SellerSerializer
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
