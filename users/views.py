dd20e576042ab2e75253fd96189ac3b0e97a1383
victorglim <victor.guedes888@gmail.com>
2023-05-06 22:34:10 -0300

    feat: imported the dependencies to build the user views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import User, Address
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics, serializers
+from .serializers import UserSerializer, AddressSerializer
+from .permissions import IsAccountOwnerOrAdmin, IsAdminToReadLists
 
-# Create your views here.
+
+
+
