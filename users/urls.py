9a053b4e4d4ad66307d281bf7e72b2b730b4f158
victorglim <victor.guedes888@gmail.com>
2023-05-06 22:48:32 -0300

    feat: created all urlpatterns directories that will be used in the default Django routes, and the views were correctly instantiated with the implementation of login

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
+
+urlpatterns = [
+    path("users/", views.UserView.as_view()),
+    path("users/<int:pk>/", views.UserDetailView.as_view()),
+    path("users/<int:pk>/", views.AddressDetailView.as_view()),
+    path("login/", jwt_views.TokenObtainPairView.as_view())
+]
