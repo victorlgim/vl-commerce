3732c4030b53b393a3ee98ecd647438ce1e14738
victorglim <victor.guedes888@gmail.com>
2023-05-07 20:27:57 -0300

    feat: product serializer created by instantiating the correct fields as well as the extra kwargs

--- products/serializers.py
+++ products/serializers.py
@@ -7,3 +7,25 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            "stock",
+            "is_available",
+            "price",
+            "description",
+            "img",
+            "categories",
+            "seller",
+        ]
+
+        extra_kwargs = {
+            "img": {
+                "read_only": True
+            }
+        }
