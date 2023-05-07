9d2bfcfc93a40734a5507805b5ca5f62bf32a00f
victorglim <victor.guedes888@gmail.com>
2023-05-07 19:05:07 -0300

    feat: added the relationship name to the products entity

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     img = models.CharField(max_length=200, blank=True)
     inventory = models.IntegerField()
     price = models.FloatField()
-    seller = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    seller = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="products")
 
 
 class Category(models.Model):
