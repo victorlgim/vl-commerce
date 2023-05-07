adc7ee3af8ec5159a2db0ee630053e052eb1462c
victorglim <victor.guedes888@gmail.com>
2023-05-06 22:42:20 -0300

    feat: finished all user views, enabling register and modify of data based on the respectve user functions and behaviors based on authentication levels

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from .models import User, Address
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import generics, serializers
-from .serializers import UserSerializer, AddressSerializer
+from .serializers import UserSerializer
 from .permissions import IsAccountOwnerOrAdmin, IsAdminToReadLists
 
 
@@ -12,5 +12,16 @@ class UserView(generics.ListCreateAPIView):
     serializer_class = UserSerializer
 
 
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwnerOrAdmin]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-
+    def perform_update(self, serializer):
+        return (
+            serializers.ValidationError({"message": "Unauthorized request"}, status=401)
+            if not self.request.user.is_superuser
+            and "is_seller" in serializer.validated_data
+            else serializer.save()
+        )
