32954740bc71f470732bca89b49560415a1cfe1d
victorglim <victor.guedes888@gmail.com>
2023-05-06 22:02:12 -0300

    feat: added the initial structure of the address serializer and instantiated all fields of the address model that will relate to the user table

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,21 @@
 from rest_framework import serializers
 from .models import User, Address
 
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = [
+            "id",
+            "zip_code",
+            "state",
+            "city",
+            "neighborhood",
+            "number",
+            "street"
+        ]
+        
+        extra_kwargs = {
+            "id": {
+                "read_only": True
+            }
+        }
