49890bc8eb814f4cd0f94ce37a09aa6b71c28855
victorglim <victor.guedes888@gmail.com>
2023-05-08 16:39:44 -0300

    fix: the issue causing problems in the cart route has been fixed, and the class name has been reformulated to be more semantically meaningful for its function

--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CartView, CartProductsView, CartProductDetailView
+from .views import CartView, CartProductsView, CartProductDeleteView
 
 urlpatterns = [
     path("carts/", CartView.as_view()),
     path("carts/<int:pk>/", CartProductsView.as_view()),
-    path("carts/product/<int:product_id>/", CartProductDetailView.as_view()),
+    path("carts/product/<int:product_id>/", CartProductDeleteView.as_view()),
 ]
