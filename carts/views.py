ab6ea6e57167f5223975447cd53c317e66f2d88a
victorglim <victor.guedes888@gmail.com>
2023-05-08 16:17:30 -0300

    feat: a function has been added without generic views that validates if the user owns the cart to be deleted, it checks if the cart exists, and if not, it allows for deletion

--- carts/views.py
+++ carts/views.py
@@ -5,6 +5,8 @@ from rest_framework import generics
 from .serializers import CartProductsSerializer
 from .permissions import IsOwner
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView, Response, status
+from users.models import User
 
 
 class CartView(generics.ListAPIView):
@@ -36,3 +38,24 @@ class CartProductsView(generics.CreateAPIView):
             serializer.save(
                 cart=self.request.user.cart, product=product, seller=product.seller
             )
+
+class CartProductsDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsOwner]
+
+    def delete(self, request, product_id):
+        user = get_object_or_404(User, id=request.user.id)
+        cart = get_object_or_404(Cart, id=user.cart.id)
+
+        cart_product = CartProducts.objects.filter(
+            cart_id=cart.id, product_id=product_id
+        ).first()
+
+        if not cart_product:
+            return Response(
+                {"message": "Product not found in cart"}, status.HTTP_404_NOT_FOUND
+            )
+
+        cart_product.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
