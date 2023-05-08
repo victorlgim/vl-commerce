c42780231df89e6e5398f6865a5e717b7ce444ae
victorglim <victor.guedes888@gmail.com>
2023-05-08 15:39:01 -0300

    feat: a view has been added that allows creation based on authentication if the authenticated user is the owner of the cart

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,4 @@
+from products.models import Product
 from .models import CartProducts, Cart
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import generics
@@ -16,3 +17,22 @@ class CartView(generics.ListAPIView):
         cart = get_object_or_404(Cart, id=self.request.user.cart.id)
 
         return CartProducts.objects.filter(cart_id=cart.id)
+    
+class CartProductsView(generics.CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsOwner]
+
+    queryset = CartProducts.objects.all()
+    serializer_class = CartProductsSerializer
+
+    def perform_create(self, serializer):
+        self.check_object_permissions(self.request, self.request.user)
+        product = get_object_or_404(Product, pk=self.kwargs["pk"])
+
+        if not Cart.objects.filter(client_id=self.request.user.id).exists():
+            cart = Cart.objects.create(client=self.request.user)
+            serializer.save(cart=cart, product=product, seller=product.seller)
+        else:
+            serializer.save(
+                cart=self.request.user.cart, product=product, seller=product.seller
+            )
