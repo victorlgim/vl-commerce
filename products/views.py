f3ddef1de63f43351d7b97062f21371acf282912
victorglim <victor.guedes888@gmail.com>
2023-05-07 22:39:06 -0300

    feat: created the "get_queryset" logic that allows capturing objects through query filtering

--- products/views.py
+++ products/views.py
@@ -11,3 +11,18 @@ class ProductView(generics.ListCreateAPIView):
     permission_classes = [IsAdminOrSellerOrReadOnly]
     queryset = Product.objects.all().order_by("id")
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        name = self.request.query_params.get("name")
+        category = self.request.query_params.get("category")
+
+        if name:
+            return Product.objects.filter(name__icontains=name)
+
+        if category:
+            return Product.objects.filter(categories__name__icontains=category)
+
+        return super().get_queryset()
+
+    def perform_create(self, serializer):
+        serializer.save(seller=self.request.user)
