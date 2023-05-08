53f93db1b140eda0710cff8fd9ac142d859ead5e
victorglim <victor.guedes888@gmail.com>
2023-05-08 14:16:54 -0300

    feat: the product cart serializer has been updated to include the quantity of items based on the products, as well as the cart entity itself for filter accuracy, this includes a verification that if the product stock is less than the quantity of items, a 400 error will be triggered

--- carts/serializers.py
+++ carts/serializers.py
@@ -19,3 +19,19 @@ class CartProductsSerializer(serializers.ModelSerializer):
         model = CartProducts
         fields = ["id", "product", "cart", "quantity", "seller"]
         read_only_fields = ["id", "cart", "product", "seller"]
+
+    def create(self, validated_data):
+        cart = validated_data["cart"]
+        product = validated_data["product"]
+        quantity = validated_data["quantity"]
+
+        if CartProducts.objects.filter(cart=cart, product=product).exists():
+            cart_product = CartProducts.objects.get(cart=cart, product=product)
+            quantity += cart_product.quantity
+
+        if product.stock < quantity:
+            raise serializers.ValidationError(
+                {"message": "Insufficient stock"}, status.HTTP_400_BAD_REQUEST
+            )
+
+        return CartProducts.objects.create(**validated_data, quantity=quantity)
