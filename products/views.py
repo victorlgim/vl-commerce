4da7a30e4bb13fdd8ea72b3d8a6f502763adea9b
victorglim <victor.guedes888@gmail.com>
2023-05-07 22:20:27 -0300

    feat: implemented the product authentication version that allows viewing the logged-in user type, making it possible to sort all products by id for pagination

--- products/views.py
+++ products/views.py
@@ -4,3 +4,10 @@ from .models import Product
 from .serializers import ProductSerializer
 from .permissions import IsAdminOrProductSeller
 from users.permissions import IsAdminOrSellerOrReadOnly
+
+
+class ProductView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminOrSellerOrReadOnly]
+    queryset = Product.objects.all().order_by("id")
+    serializer_class = ProductSerializer
