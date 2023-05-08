4ffdfb8f6125318a350e0b6e5811fd1707bb9775
victorglim <victor.guedes888@gmail.com>
2023-05-07 22:18:52 -0300

    chore: imported the necessary dependencies to build the product views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Product
+from .serializers import ProductSerializer
+from .permissions import IsAdminOrProductSeller
+from users.permissions import IsAdminOrSellerOrReadOnly
