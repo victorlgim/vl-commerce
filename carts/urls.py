dcfb32082a57d0f8f6de9b1e60bffd041bf71c2e
victorglim <victor.guedes888@gmail.com>
2023-05-08 16:23:44 -0300

    fix: the import route with the incorrect value has been corrected

--- carts/urls.py
+++ carts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CartView, CartProductsView
+from .views import CartView, CartProductsView, CartProductDetailView
 
 urlpatterns = [
     path("carts/", CartView.as_view()),
-    path("carts/<int:pk>/", CartProductsView.as_view())
+    path("carts/<int:pk>/", CartProductsView.as_view()),
+    path("carts/product/<int:product_id>/", CartProductDetailView.as_view()),
 ]
