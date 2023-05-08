38366719a3c1255abbd8695342aea3ead7864183
victorglim <victor.guedes888@gmail.com>
2023-05-08 14:41:19 -0300

    feat: a view for the product cart has been added, which allows the cart to be listed based on the user id

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .models import CartProducts, Cart
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics
+from .serializers import CartProductsSerializer
+from .permissions import IsOwner
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class CartView(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsOwner]
+
+    serializer_class = CartProductsSerializer
+
+    def get_queryset(self):
+        cart = get_object_or_404(Cart, id=self.request.user.cart.id)
+
+        return CartProducts.objects.filter(cart_id=cart.id)
