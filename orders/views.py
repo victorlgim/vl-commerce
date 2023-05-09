2899098b537dbba7ced98a9b59298f6e499b9bb1
victorglim <victor.guedes888@gmail.com>
2023-05-09 15:17:50 -0300

    feat: created a view using generic views that enables listing all orders from the own customer, or the seller and their respective orders, or the administrator

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import OrderSerializer
+from .models import Order
+from rest_framework import generics
+from carts.permissions import IsOwner
+from .permissions import IsAdminOrSeller
 
-# Create your views here.
+
+class OrderView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsOwner, IsAdminOrSeller]
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def perform_create(self, serializer):
+        self.check_object_permissions(self.request, self.request.user)
+        serializer.save(client_id=self.request.user.id)
