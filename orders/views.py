89b4f23604f521d76ed353988ca6ecd504215411
victorglim <victor.guedes888@gmail.com>
2023-05-09 15:51:48 -0300

    feat: priority information view and resource control, such as product deletion or modification with authentication only by the administrator or product owner, have been built

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ from .serializers import OrderSerializer
 from .models import Order
 from rest_framework import generics
 from carts.permissions import IsOwner
-from .permissions import IsAdminOrSeller
+from .permissions import IsAdminOrSeller, IsProductSeller, IsAdminOrSellerOrOwner
 
 
 class OrderView(generics.ListCreateAPIView):
@@ -16,3 +16,13 @@ class OrderView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         self.check_object_permissions(self.request, self.request.user)
         serializer.save(client_id=self.request.user.id)
+
+
+class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsProductSeller, IsAdminOrSellerOrOwner]
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    lookup_url_kwarg = "order_id"
