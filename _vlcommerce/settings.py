6bc54ad938953ad4ea54d2677df05d863d4ecace
victorglim <victor.guedes888@gmail.com>
2023-05-09 00:16:42 -0300

    feat: added the variable email sending scope to the django settings

--- _vlcommerce/settings.py
+++ _vlcommerce/settings.py
@@ -145,3 +145,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST
+EMAIL_PORT
+EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD
