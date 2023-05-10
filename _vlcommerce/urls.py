04bf916ef7029fd3fc994aaa62263f975f4b818b
victorglim <victor.guedes888@gmail.com>
2023-05-09 21:06:56 -0300

    chore: added the route for orders within the urls in settings.py

--- _vlcommerce/urls.py
+++ _vlcommerce/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("api/", include("users.urls")),
     path("api/", include("products.urls")),
     path("api/", include("carts.urls")),
+    path("api/", include("orders.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path("api/docs/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui")
 ]
